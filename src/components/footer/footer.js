c8f300603bcf64371bf553d9ec14e75fd8f18312
Nomad9-0 <ojimenezlmpc@gmail.com>
2020-01-12 15:16:04 -0800

    navbar created

---
+++ src/components/footer/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footrt.css';
+
+class Footer extends React.Component {
+    render(){
+        return (
+            <div className="App">
+            <h1>KT Front</h1>
+            </div>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './navbar.css';
+
+class Navbar extends React.Component {
+    render(){
+        
+        return (
+            <div className="Nav">
+                <div className="topnav" id="navbar">
+                    <a href="#home">KT FRONT</a>
+                    <a href="#muster">MUSTER</a>
+                    <a href="#myKt">MY KILL TEAM</a>
+                    <a href="#contact">CONTACT</a>
+                    <i id="responsiveToggle" class="fas fa-bars responsive-nav"></i>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
