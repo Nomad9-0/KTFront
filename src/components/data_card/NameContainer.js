390db5f6b59e7fa79f811d79506adb410d883ece
Nomad9-0 <ojimenezlmpc@gmail.com>
2020-03-08 17:58:30 -0700

    Data Card Component

---
+++ src/components/data_card/NameContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NameContainer.css';
+
+class NameContainer extends React.Component  {
+  render(){
+    return (
+      <div className="NameContainer">
+        
+      </div>
+    );
+  }
+}
+
+export default NameContainer;
