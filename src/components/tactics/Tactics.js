66f884ef48998f5e262e2d9b53798eaeef458430
Nomad9-0 <ojimenezlmpc@gmail.com>
2020-03-30 19:55:03 -0700

    added react-tabs package

--- src/components/tactics/Tactics.js
+++ src/components/tactics/Tactics.js
@@ -1,57 +1,35 @@
 import React from 'react';
 import TacticsCard from './TacticsCard';
 import './Tactics.css';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 class Tactics extends React.Component  {
 
   render(){
-    function openTactic(evt, tacticSection) {
-        // Declare all variables
-        const tabContent = document.getElementsByClassName("tabContent");
-        const tabLink = document.getElementsByClassName("tabLink");
-        let i;
-      
-        // Get all elements with class="tabcontent" and hide them
-        for (i = 0; i < tabContent.length; i++) {
-          tabContent[i].style.display = "none";
-        }
-        // Get all elements with class="tablinks" and remove the class "active"
-        for (i = 0; i < tabLink.length; i++) {
-          tabLink[i].className = tabLink[i].className.replace(" active", "");
-        }
-      
-        // Show the current tab, and add an "active" class to the button that opened the tab
-        document.getElementById(tacticSection).style.display = "block";
-        evt.currentTarget.className += " active";
-    }
     return (
       <div className="Tactics">
         <h2>Tactics</h2>
-        <div className='tacticsTab'>
-            <button className='core_tactics_tab tabLink' onClick={openTactic}>Core Tactics</button>
-            <button className='specialist_tactics_tab tabLink' onClick={openTactic}>Specialist Tactics</button>
-            <button className='faction_tactics_tab tabLink' onClick={openTactic}>Faction Tactics</button>
-        </div>
-         
-        <div id="core_tactics" className="tabContent">
-            <div id="tacticsDisplay">
-                <TacticsCard />
-                <TacticsCard />
-                <TacticsCard />
-            </div>
-        </div>
-        <div id="specialist_tactics" className="tabContent">
-            <div id="tacticsDisplay">
-                <TacticsCard />
-                <TacticsCard />
-            </div>
-        </div>
-        <div id="faction_tactics" className="tabContent">
-            <div id="tacticsDisplay">
-                <TacticsCard />
-            </div>
-        </div>  
-        
+        <Tabs>
+          <TabList className='tabNames'>
+            <Tab>Core</Tab>
+            <Tab>Specialist</Tab>
+            <Tab>Faction</Tab>
+          </TabList>
+      
+          <TabPanel className='tabBody' id='coreTactics'>
+            <TacticsCard />
+          </TabPanel>
+          <TabPanel className='tabBody' id='specialistTacttics'>
+            <TacticsCard />
+            <TacticsCard />
+          </TabPanel>
+          <TabPanel className='tabBody' id='factionTactics'>
+            <TacticsCard />
+            <TacticsCard />
+            <TacticsCard />
+          </TabPanel>
+        </Tabs>   
       </div>
     );
   }
