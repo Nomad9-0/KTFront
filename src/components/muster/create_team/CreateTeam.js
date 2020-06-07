c374f238115e5731ac47bb151e5de34e429a5920
Nomad9-0 <ojimenezlmpc@gmail.com>
2020-06-07 13:15:37 -0700

    Team creation layout

--- src/components/muster/create_team/CreateTeam.js
+++ src/components/muster/create_team/CreateTeam.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import UnitData from '../../unit_data/UnitData';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import "./CreateTeam.css";
 
 function CreateTeam() {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
     return (
         <>
         <div className="jumbotron">
@@ -10,13 +11,32 @@ function CreateTeam() {
           <p className="description">Build Your Roster</p>
         </div>
         <div className='memberList'>
-            <UnitData />
+            
         </div>
         <div className='addRemoveContainer'>
-            <button className='btn btn-primary addMember'>Add Member</button>
+            <button className='btn addMember' onClick={() => setModalIsOpen(true)}>Add Member</button>
             <button className='btn btn-warning removeMember'>Remove Member</button>
         </div>
         
+        <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+            <button className='btn btn-danger addMemberModalClose' onClick={() => setModalIsOpen(false) }>X</button>
+            <h3 className='addMemberModalTitle'>Add a Team Member</h3>
+            {/* This will be updated dynamicaly */}
+            <ul>
+                <div>
+                    <p>Infantry Squad Guardsmen</p>
+                    <li>Sergeant</li>
+                    <li>Guardsmen</li>
+                    <li>Guardsmen Gunner</li>
+                </div>
+                <div>
+                    <p>Scion</p>
+                    <li>Scion</li>
+                    <li>Tempestor</li>
+                    <li>Scion Gunner</li>
+                </div>
+            </ul>
+        </Modal>
         </>
     );
 }
